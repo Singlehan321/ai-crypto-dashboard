8acc29fac9a07dc5fed73563091caf547b20aaf2
Singlehan321 <136765771+Singlehan321@users.noreply.github.com>
2026-05-18 14:23:33 +0330

    feat: connect dashboard components

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("AI Crypto Dashboard Started")
+from utils.api import fetch_price
+from utils.signals import generate_signal
+from utils.formatter import format_price
+
+data = fetch_price("bitcoin")
+
+price = data["bitcoin"]["usd"]
+
+signal = generate_signal(price)
+
+formatted = format_price(price)
+
+print("BTC Price:", formatted)
+print("Signal:", signal)
